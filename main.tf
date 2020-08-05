d04b1e253a30ad2113809fe76257fdd551d5a6ca
karollynecosta <karolcosta2@gmail.com>
2020-08-05 15:34:02 -0300

    3

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ provider "aws" {
   region  = "us-east-1"
   version = "~> 2.0"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "dev-terraform-karollyne"
+    key = "terraform-init.tfstate"
+    region = "us-east-1"
+  }
+}
